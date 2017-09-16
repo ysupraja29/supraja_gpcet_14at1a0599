646fbcc19adff56f9246f73af02c78a9f978a827
ysupraja29 <30411542+ysupraja29@users.noreply.github.com>
2017-09-16 13:48:29 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Student s =new student();
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
